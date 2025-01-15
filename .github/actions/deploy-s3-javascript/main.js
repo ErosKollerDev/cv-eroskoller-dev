3604bb5b1db89e28ed06acd6680c15b613c2f2bd
eros <eroskoller@gmail.com>
2025-01-15 11:07:36 -0300

    Adding GitHub Action to project

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+
+function run() {
+    core.info('Deploying to S3');
+    core.notice('Starting process to deploy to S3');
+    // 1) Get inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload to S3
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+    //https://gh-custom-action-static-website-eros.s3-website-us-east-1.amazonaws.com
+    //https://gh-custom-action-static-website-eros.s3-website-us-east-1.amazonaws.com/
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+}
+
+
+
+run();
